7fc2b66717484485ef978d14ba5b9754eac2ff06
FajarSant <4jar100so@gmail.com>
2023-10-27 21:43:42 +0700

    Frist

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import aku from '../Assets/aku.jpg';
+import aku from '../Assets/aku1.jpg';
 import TypewriterText from '../Components/TypeWriterText';
-import CV from "../Assets/aku.jpg"
 
 function Home() {
   const handleDownloadCV = () => {
@@ -20,32 +19,32 @@ function Home() {
 
   return (
     <main className="welcome-container mt-10 md:mt-1 flex flex-col-reverse gap-8 items-center md:flex-row md:gap-16 md:justify-center min-h-[65vh] md:min-h-[80vh]">
-      <div className="welcome-text-container space-y-2 text-center md:text-left px-10">
+      <div className="welcome-text-container space-y-2 text-center md:text-left px-4 md:px-10"> {/* Perubahan pada padding */}
         <p className="text-black">Hello World, I'm</p>
-        <h1 className="text-4xl font-bold md:text-5xl text-black fadein-up">Bagas Rakha</h1>
+        <h1 className="text-2xl md:text-4xl font-bold text-black fadein-up">Bagas Rakha</h1>
         <div className="py-2">
-          <h1 className="typewrite text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-black to-yellow-500 md:text-2xl fadein-up">
+          <h1 className="typewrite text-xl md:text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-black to-yellow-500 fadein-up">
             <span className="wrap"><TypewriterText /></span>
           </h1>
         </div>
         <p className="text-black pr-4 fade-in-from-left animate__animated animate__fadeIn">
           Welcome to My personal website. <span role="img" aria-label="wave">👋🏼</span>
         </p>
-        <div className="welcome-buttons">
+        <div className="welcome-buttons flex flex-col md:flex-row gap-4 md:gap-0"> {/* Perubahan pada tata letak tombol */}
           <button className="btn glass" onClick={handleDownloadCV}>
             Download CV
           </button>
-          <button className="btn glass" onClick={handleContactMe}>
+          <button className="btn glass p-2" onClick={handleContactMe}> {/* Perubahan pada padding */}
             Contact Me
           </button>
         </div>
       </div>
-      <div className="welcome-image-container flex justify-center md:justify-start fadein-right">
+      <div className="welcome-image-container flex justify-center md:justify-start fadein-right ">
         <img
           alt="avatar"
-          width="300"
-          height="300"
-          className="w-10/11 md:h-auto rounded-full border-4 border-amber-200 pict"
+          width="100"
+          height="100"
+          className="w-8/12  rounded-full border-4 border-amber-200 pict"
           src={aku}
         />
       </div>
--- src/Home/Navigasi.js
+++ src/Home/Navigasi.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaGithub, FaLinkedin } from 'react-icons/fa'; // Impor ikon GitHub dan LinkedIn
 
 function Navigasi() {
-  // const navStyle = {
-  //   background: 'rgba(0, 0, 0, 0.55)', // Slightly transparent background
-  //   backgroundColor: 'rgba(255, 255, 255, 0.7)', // Latar belakang transparan dengan alpha 0.7
-  //   backdropFilter: 'blur(10px)', // Efek blur dengan radius 10px
-  // };
-
   return (
-    <nav  className="bg-base-300 p-4 fixed top-0 left-0 right-0 z-10">
+    <nav className="bg-base-300 p-4 fixed top-0 left-0 right-0 z-10">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center"> {/* Bagian kiri */}
+        <div className="flex items-center">
           <NavLink to="/" className="text-black text-2xl font-semibold">Fajar Santoso#</NavLink>
         </div>
 
-        <ul className="flex space-x-4"> {/* Bagian tengah */}
+        <ul className="flex space-x-4">
           <li>
-            <NavLink to="/" className="text-gray-300 hover:text-black">Home</NavLink>
+            <NavLink to="/" className="text-gray-600 hover:text-black">Home</NavLink>
           </li>
           <li>
-            <NavLink to="/Biodata" className="text-gray-300 hover:text-black">Biodata</NavLink>
+            <NavLink to="/Biodata" className="text-gray-600 hover:text-black">Biodata</NavLink>
           </li>
           <li>
-            <NavLink to="/Portofolio" className="text-gray-300 hover:text-black">Portofolio</NavLink>
+            <NavLink to="/Portofolio" className="text-gray-600 hover:text-black">Portofolio</NavLink>
           </li>
           <li>
-            <NavLink to="/Skills" className="text-gray-300 hover:text-black">Skills</NavLink>
+            <NavLink to="/Skills" className="text-gray-600 hover:text-black">Skills</NavLink>
           </li>
         </ul>
 
-        <div className="flex items-center space-x-4"> {/* Bagian kanan */}
-          <NavLink to="/profil" className="text-white hover:text-black" activeClassName="active">Profil</NavLink>
+        <div className="flex items-center space-x-4">
+          <a href="https://github.com/username" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="text-gray-600 hover:text-black" size={24} /> {/* Ikon GitHub */}
+          </a>
+          <a href="https://www.linkedin.com/in/username" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="text-gray-600 hover:text-black" size={24} /> {/* Ikon LinkedIn */}
+          </a>
         </div>
       </div>
     </nav>
